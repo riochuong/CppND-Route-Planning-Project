ead676c1f6cc9b9e8d9f2a8586004e96e50c7b53
chuongdao <cd.kernel.dev.105@gmail.com>
2019-05-12 15:48:50 -0700

    add mnodes to new RouteModel

--- src/route_model.h
+++ src/route_model.h
@@ -3,8 +3,9 @@
 #include <limits>
 #include <cmath>
 #include <unordered_map>
-#include "model.h"
 #include <iostream>
+
+#include "model.h"
 
 class RouteModel : public Model {
 
@@ -15,12 +16,20 @@ class RouteModel : public Model {
         
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
-      
+        
+        Node *parent = nullptr;
+        // heuristic values for A* planner
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0.0;
+        bool visited = false;
+        std::vector<Node> *neighbors = nullptr;
       private:
         // Add private Node variables and methods here.
         int index;
         RouteModel * parent_model = nullptr;
     };
+
+    std::vector<RouteModel::Node> const &SNodes() const { return this->m_Nodes; }
     
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
@@ -28,5 +37,6 @@ class RouteModel : public Model {
 
   private:
     // Add private RouteModel variables and methods here.
+    std::vector<RouteModel::Node> m_Nodes;
 
 };
