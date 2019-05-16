d26bec71cef39df0ca219a1d623c4cbe909c79fe
chuongdao <cd.kernel.dev.105@gmail.com>
2019-05-16 00:22:58 -0700

    add user inputs feature

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include "route_planner.h"
 
 using namespace std::experimental;
-
+const float DIFF = 0.00001;
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
     std::ifstream is{path, std::ios::binary | std::ios::ate};
@@ -27,8 +27,55 @@ static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
     return std::move(contents);
 }
 
+void PromptForInput(float *start_x, float *start_y, float *end_x, float *end_y) {
+    int x0;
+    int x1;
+    int y0;
+    int y1;
+    while(true) {
+        std::cout << "Please input start x position in the range 0-100: ";
+        if (std::cin >> x0 && x0 >= 0 && x0 <= 100) {
+            *start_x = x0;
+            break;
+        }
+        std::cin.clear();
+        std::cout << "Invalid input value " <<  std::endl;
+    }
+    while(true) {
+        std::cout << "Please input start y position in the range 0-100: ";
+        if (std::cin >> y0 && y0 >= 0 && y0 <= 100) {
+            *start_y = y0;
+            break;
+        }
+        std::cin.clear();
+        std::cout << "Invalid input value !!" << std::endl;
+    }
+    while(true) {
+        std::cout << "Please input end x position in the range 0-100: ";
+        if (std::cin >> x1 && x1 >= 0 && x1 <= 100) {
+            *end_x = x1;
+            break;
+        }
+        std::cin.clear();
+        std::cout << "Invalid input value !!" << std::endl;
+    }
+    while(true) {
+         std::cout << "Please input end y position in the range 0-100: ";
+        if (std::cin >> y1 && y1 >= 0 && y1 <= 100) {
+            *end_y = y1;
+            break;
+        }
+        std::cin.clear();
+        std::cout << "Invalid input value !!" << std::endl;
+    }
+}
+
 int main(int argc, const char **argv)
 {    
+    float start_x;
+    float start_y;
+    float end_x;
+    float end_y;
     std::string osm_data_file = "";
     if( argc > 1 ) {
         for( int i = 1; i < argc; ++i )
@@ -36,7 +83,8 @@ int main(int argc, const char **argv)
                 osm_data_file = argv[i];
     }
     else {
-        std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;    
+        std::cout << "Usage: [executable] [-f filename.osm]" << std::endl; 
+        return -1;   
     }
     
     std::vector<std::byte> osm_data;
@@ -50,15 +98,15 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
     
-    // TODO: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below.
+    // prompt to get user input
+    
+    PromptForInput(&start_x, &start_y, &end_x, &end_y);
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Perform search and render results.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
     
     Render render{model};
